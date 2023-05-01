c064a60d5c53f8f4922ead9ac4ed403a14465080
mathewrasband <58843063+mathewrasband@users.noreply.github.com>
2023-05-01 11:02:45 -0600

    Update VideoService.java
    Added VideoRepository

--- Backend/Daflix2.0/src/main/java/com/daflix20/service/VideoService.java
+++ Backend/Daflix2.0/src/main/java/com/daflix20/service/VideoService.java
@@ -3,11 +3,24 @@ package com.daflix20.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.daflix20.model.Video;
+import com.daflix20.repository.VideoRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class VideoService {
 	
+	private S3Service s3Service;
+	private VideoRepository videoRepository;
+	
 	public void uploadVideo(MultipartFile multipartFile) {
-		// Upload FIle to AWS S3
-		// Save Video Data to Database
+		
+		String videoUrl = s3Service.uploadFile(multipartFile);
+		var video = new Video();
+		video.setVideoUrl(videoUrl);
+		
+		videoRepository.save(video);
 	}
 }
