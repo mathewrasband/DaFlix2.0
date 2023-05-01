fbbe800851460352dcb189d96d9a2dfd5f5c2373
mathewrasband <58843063+mathewrasband@users.noreply.github.com>
2023-05-01 08:58:12 -0600

    initial commit

---
+++ Frontend/Daflix-2.0-UI/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Daflix-2.0-UI';
+}
