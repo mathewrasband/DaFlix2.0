43efca956003420d5c9ed85ac7a9c11ca0e1d317
mathewrasband <58843063+mathewrasband@users.noreply.github.com>
2023-05-01 15:13:26 -0600

    Created UploadVideoResponse Interface and updated Video Service

--- Frontend/Daflix-2.0-UI/src/app/video.service.ts
+++ Frontend/Daflix-2.0-UI/src/app/video.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { UploadVideoResponse } from './upload-video/UploadVideoResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,12 @@ export class VideoService {
 
   constructor(private httpClient: HttpClient) { }
 
-  uploadVideo(fileEntry: File): Observable<any>{
+  uploadVideo(fileEntry: File): Observable<UploadVideoResponse>{
 
     const formData = new FormData();
     formData.append('file', fileEntry, fileEntry.name);
 
     // HTTP Post call to upload the video
-    return this.httpClient.post("http:/localhost:8080/api/videos", formData);
+    return this.httpClient.post<UploadVideoResponse>("http:/localhost:8080/api/videos", formData);
   }
 }
