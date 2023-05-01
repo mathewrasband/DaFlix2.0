c7928f44536844e6c1e2c220aa3ccc7adbbb8911
mathewrasband <58843063+mathewrasband@users.noreply.github.com>
2023-05-01 14:18:06 -0600

    Update WebMvcConfig.java

--- Backend/Daflix2.0/src/main/java/com/daflix20/config/WebMvcConfig.java
+++ Backend/Daflix2.0/src/main/java/com/daflix20/config/WebMvcConfig.java
@@ -1,7 +1,17 @@
 package com.daflix20.config;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		corsRegistry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+			.allowedHeaders("*")
+			.maxAge(3600);
+	}
 
 }
