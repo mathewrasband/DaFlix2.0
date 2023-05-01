3087d066736159b2ac341eb4044a3e677d81d134
mathewrasband <58843063+mathewrasband@users.noreply.github.com>
2023-05-01 11:09:05 -0600

    Update VideoController.java
    Autowired VideoService and finished the uploadVideo method.

--- Backend/Daflix2.0/src/main/java/com/daflix20/controllers/VideoController.java
+++ Backend/Daflix2.0/src/main/java/com/daflix20/controllers/VideoController.java
@@ -8,14 +8,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.daflix20.service.VideoService;
+
 @RestController
 @RequestMapping("/api/videos")
 public class VideoController {
 	
+	private VideoService videoService;
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void uploadVideo(@RequestParam("file") MultipartFile file) {
-		
+		  videoService.uploadVideo(file);
 	}
 
 }
